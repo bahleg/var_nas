Fixed search. Currently it's equals to pt.darts + fix bug with hessian computation

models/cnn/one_hot_cnn.py
@@ -120,7 +120,7 @@ class OneHotSearchCNNController(SearchCNNController):
 
     def init_net(self, kwargs):
         subcfg = kwargs['darts']
-        drop = float(subcfg['drop path proba'])
+        #drop = float(subcfg['drop path proba'])
         C_in = int(subcfg['input_channels'])
         C = int(subcfg['init_channels'])
         n_classes = int(subcfg['n_classes'])
@@ -135,9 +135,17 @@ class OneHotSearchCNNController(SearchCNNController):
         
         if self.aux > 0.0:
             self.aux_pos = 2*n_layers//3
-            
+        
+        self.drop_init = float(subcfg['drop path proba initial'])
+        self.drop_delta = float(subcfg['drop path proba delta'])
+        
+        if self.drop_init > 0 or self.drop_delta != 0:
+            self.drop = torch.tensor(0.0001) # need to initialize cells properly
+        else:
+            self.drop = 0.0
+
         self.net = OneHotCNN(primitives, C_in, C, n_classes, n_layers,
-                             n_nodes, stem_multiplier, drop, self.aux, input_size)
+                             n_nodes, stem_multiplier, self.drop, self.aux, input_size)
     
     def __init__(self, **kwargs):        
         SearchCNNController.__init__(self, **kwargs)
@@ -177,3 +185,11 @@ class OneHotSearchCNNController(SearchCNNController):
             return self.aux *  self.criterion(aux_logits, y) +  self.criterion(logits, y)
         return self.criterion(logits, y)
     
+
+    def new_epoch(self, e, w, l):
+        SearchCNNController.new_epoch(self, e, w, l)
+        if self.drop_delta != 0.0 or self.drop_init > 0.0:
+             self.drop.data *= 0
+             self.drop.data += self.drop_init + self.drop_delta * e
+
+

models/cnn/search_cnn.py
@@ -87,7 +87,9 @@ class SearchCNNController(nn.Module):
 
     def init_net(self, kwargs):
         subcfg = kwargs['darts']
-        drop = float(subcfg['drop path proba'])
+        #self.drop_init = float(subcfg['drop path proba initial'])
+        #self.drop_delta = float(subcfg['drop path proba delta'])
+
         C_in = int(subcfg['input_channels'])
         C = int(subcfg['init_channels'])
         n_classes = int(subcfg['n_classes'])
@@ -96,10 +98,13 @@ class SearchCNNController(nn.Module):
         stem_multiplier = int(subcfg['stem_multiplier'])
         
         primitives = self.get_primitives(kwargs)
-
-
+        #if self.drop_init > 0 or self.drop_delta != 0:
+        #    self.drop = torch.tensor(0.0001) # need to initialize cells properly
+        #else:
+        #    self.drop = 0.0
+          
         self.net = SearchCNN(primitives, C_in, C, n_classes, n_layers,
-                             n_nodes, stem_multiplier, drop)
+                             n_nodes, stem_multiplier)
                              
     def __init__(self, **kwargs):
         super().__init__()    
@@ -132,6 +137,7 @@ class SearchCNNController(nn.Module):
         self.architect = Architect(self, float(
             subcfg['optim']['w_momentum']), float(subcfg['optim']['w_weight_decay']))
         self.w_grad_clip = float(subcfg['optim']['w_grad_clip'])
+        self.epochs = int(kwargs['epochs'])
 
     def train_step(self, trn_X, trn_y, val_X, val_y):
         lr = self.lr_scheduler.get_last_lr()[0]
@@ -318,6 +324,9 @@ class SearchCNNController(nn.Module):
         self.lr_scheduler.step(epoch=e)
         self.t = self.init_t + self.delta_t*e
         self.t = torch.tensor(self.t).to(self.device)
+        #if self.drop_delta != 0.0 or self.drop_init > 0.0:
+        #     self.drop.data *= 0
+        #     self.drop_data += self.drop_init + self.drop_delta * e
 
 
     def writer_callback(self, writer,  epoch, cur_step):

search.py
@@ -48,7 +48,6 @@ def main():
         model_params['seed'] = seed
         model = controller_cls(**model_params)
         model = model.to(device)    
-
         # split data to train/validation
         n_train = len(train_data)
         
@@ -74,12 +73,12 @@ def main():
         
         else:
             logger.info('using test as validation. Use it only for the training with already defined architecture!')
-        train_loader = torch.utils.data.DataLoader(train_data,
+            train_loader = torch.utils.data.DataLoader(train_data,
                                                batch_size=int(config.batch_size),
                                                shuffle=True,
                                                num_workers=int(config.workers),
                                                pin_memory=True)
-        valid_loader = torch.utils.data.DataLoader(valid_data,
+            valid_loader = torch.utils.data.DataLoader(valid_data,
                                                batch_size=int(config.batch_size),
                                                shuffle=False,
                                                num_workers=int(config.workers),
@@ -106,7 +105,12 @@ def main():
 
             model.new_epoch(epoch, writer, logger) 
             # training
-            train_qual = train(train_loader, valid_loader, model, epoch, writer,  config, logger)        
+            if float(config.validate_split) <= 0.0:
+                val_train_loader = train_loader
+            else:
+                val_train_loader = valid_loader
+ 
+            train_qual = train(train_loader, val_train_loader, model, epoch, writer,  config, logger)        
                         
             # validation
             cur_step = (epoch+1) * len(train_loader)
@@ -153,7 +157,7 @@ def train(train_loader, valid_loader, model, epoch, writer,  config, logger):
         loss = model.train_step(trn_X, trn_y, val_X, val_y)
         losses.update(loss.item(), N)
 
-        if step ==  len(train_loader)-1:
+        if  step ==  len(train_loader)-1:
             model.eval()
             logits = model(trn_X)
             model.train()